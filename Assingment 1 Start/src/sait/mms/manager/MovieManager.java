44426efecf3fce25cb1c0e53fbcc0b365cfbeaa3
earlysakura-hub <earlysakura@gmail.com>
2026-02-04 20:53:07 -0700

    Update MovieManager.java

--- Assingment 1 Start/src/sait/mms/manager/MovieManager.java
+++ Assingment 1 Start/src/sait/mms/manager/MovieManager.java
@@ -18,21 +18,15 @@ public class MovieManager {
 		//System.out.println(movies.get(1).toString()); // this is just a test
 		
 		displayMenu();
-
-		
 	}
 
-		
-
 	private void displayMenu() {
-		//printing out menu
 		System.out.println("Movie Management system");
 		System.out.println("1    Add New Movie and Save");
 		System.out.println("2    Generate List of Movies Released in a Year");
 		System.out.println("3    Generate list of random Movies");
 		System.out.println("4    Exit");
 		
-		// getting input
 		System.out.print("Enter an option: ");
 		int option = Integer.parseInt(keyboard.nextLine());
 		
@@ -48,17 +42,81 @@ public class MovieManager {
 		else if (option == 4) {
 			saveMovieListToFile();			
 		}
-		
 	}
 	
 	//Lin
 	private void addMovie() {
-		
+		int duration = 0;
+		int year = 0;
+		String title = "";
+
+		System.out.print("\nEnter duration: ");
+		if (keyboard.hasNextInt()) {
+			duration = keyboard.nextInt();
+		}
+		keyboard.nextLine();
+
+		if (duration <= 0) {
+			System.out.println("Invalid duration!");
+			return;
+		}
+
+		System.out.print("Enter movie title: ");
+		title = keyboard.nextLine().trim();
+
+		if (title.isEmpty()) {
+			System.out.println("Invalid title!");
+			return;
+		}
+
+		System.out.print("Enter year: ");
+		if (keyboard.hasNextInt()) {
+			year = keyboard.nextInt();
+		}
+		keyboard.nextLine();
+
+		if (year <= 0) {
+			System.out.println("Invalid year!");
+			return;
+		}
+
+		movies.add(new Movie(duration, title, year));
+
+		System.out.println("\nSaving movies...");
+		saveMovieListToFile();
+		System.out.println("Added movie to the data file.\n");
 	}
 	
 	//Lin
 	private void generateMovieListInYear() {
-		
+		int inputYear = 0;
+
+		System.out.print("\nEnter in year: ");
+		if (keyboard.hasNextInt()) {
+			inputYear = keyboard.nextInt();
+		}
+		keyboard.nextLine();
+
+		if (inputYear <= 0) {
+			System.out.println("Invalid year!");
+			return;
+		}
+
+		System.out.println("\n\nMovie List\n");
+		System.out.println("Duration\tYear\tTitle");
+
+		int totalDuration = 0;
+
+		for (Movie m : movies) {
+			if (m.getYear() == inputYear) {
+				totalDuration += m.getDuration();
+				System.out.println(
+					m.getDuration() + "\t\t" + m.getYear() + "\t" + m.getTitle()
+				);
+			}
+		}
+
+		System.out.println("\nTotal duration: " + totalDuration + " minutes\n");
 	}
 	
 
@@ -72,7 +130,6 @@ public class MovieManager {
 	}
 
 	private void loadMovieList() {
-		//finding the text file
 		File movieFile = new File("res/movies.txt");
 		Scanner fileScanner = null;
 		try {
@@ -85,21 +142,16 @@ public class MovieManager {
 		String title = null;
 		int year;
 		
-		//looping though each line and turning it into a Movie Object
 		while (fileScanner.hasNextLine()) {
 			String movieText = fileScanner.nextLine();
-			//System.out.println(movieText);
 			
-			//splitting the text
 			String[] text = movieText.split(",");
 			duration = Integer.parseInt(text[0]);
 			title = text[1];
 			year = Integer.parseInt(text[2]);
 			
-			//creating and adding movie to the list
 			Movie movie = new Movie(duration,title,year);
 			movies.add(movie);
 		}
-
 	}
 }
