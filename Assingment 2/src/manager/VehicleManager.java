4cf7bd8e204ac966bc5961466424d23850265598
Camille Tsui <camille.tsui@edu.sait.ca>
2026-03-04 16:55:13 -0700

    Add skeleton code and switch cases for vehicle types

--- Assingment 2/src/manager/VehicleManager.java
+++ Assingment 2/src/manager/VehicleManager.java
@@ -19,12 +19,56 @@ public class VehicleManager {
 			Scanner input = new Scanner(file);
 			while (input.hasNext()) {
 				String line = input.nextLine();
-				System.out.println(line);
+				String[] tokens = line.split(";");
+				String vehicleType = tokens[1];
+				Vehicle vehicle = null;
+				//System.out.println(line);
+				//System.out.println(vehicleType);
+				
+				switch(vehicleType) // edit as needed
+				{
+					case "Hatchback":
+						vehicle = new Hatchback();
+						break;
+					case "Hybrid":
+						vehicle = new Hybrid();
+						break;
+					case "PickupTruck":
+						vehicle = new PickupTruck();
+						break;
+					case "Sedan":
+						vehicle = new Sedan();
+						break;
+					case "SUV":
+						vehicle = new SUV();
+						break;
+				}
+				vehicles.add(vehicle);
 			}
 			input.close();
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
+		
+	}
+	
+	private void displayMenu() {
+		
+	}
+	
+	private void purchaseVehicle() {
+		
+	}
+	
+	private void dispayVehiclesBySubtype() {
+		
+	}
+	
+	private void produceRandomListOfVechicles () {
+		
+	}
+	
+	private void saveAndExit () {
 		
 	}
 }
